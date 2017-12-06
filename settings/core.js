4c4a0d51cd8f0671921fc67afcb474d5acad856f
Aford <244734834@qq.com>
2017-12-06 12:09:10 +0800

    changed details

--- settings/core.js
+++ settings/core.js
@@ -8,7 +8,7 @@ module.exports = {
     env: env.prod,
     extJS: [
       '//webapi.amap.com/maps?v=1.4.0&key=f582aead07ca97967fea0637d51e98ac',
-      'core.min.js',
+      'core.min.js?v1.0.0',
       'https://cdn.bootcss.com/echarts/3.7.1/echarts.simple.min.js',
       'https://cdn.bootcss.com/axios/0.17.1/axios.min.js'
     ],
